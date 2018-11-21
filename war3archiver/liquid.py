50da55dbe82dec5d0fe5bcde265e1fd722b7a7eb
sides <sides@sides.tv>
2018-11-21 22:20:47 +0000

    liquid: Add convenience methods; os.path -> pathlib
    Considering we're dealing with WC3 maps I doubt memory is going to be
    an issue. Just keep entire contents in memory, faster, more convenient.

--- war3archiver/liquid.py
+++ war3archiver/liquid.py
@@ -1,39 +1,69 @@
 import os
+import io
+
+from pathlib import Path
 
 class Liquid():
   """Liquid class.
 
   A liquid is an in-memory representation of a file whose data can be
-  read through a stream. It should be treated as immutable.
+  read through a stream. It should be treated as immutable and a liquid
+  will typically have a short lifespan.
   """
 
   def __init__(self, source, reader=None):
     self._source = source
-    self._filename = os.path.basename(self._source)
 
-    splitext = os.path.splitext(self._filename)
-
-    self._filename_without_ext = splitext[0]
-    self._fileext = splitext[1].lstrip('.')
+    path = Path(source)
+    self._name = path.name
+    self._stem = path.stem
+    self._suffix = path.suffix.lstrip('.')
 
     self._reader = reader if not reader is None else lambda: open(self._source, 'rb')
     self._cached_contents = None
 
   @property
   def source(self):
+    """Get the liquid's source"""
     return self._source
 
   @property
-  def filename(self):
-    return self._filename
+  def name(self):
+    """Get the liquid's natural name"""
+    return self._name
 
   @property
-  def filename_without_ext(self):
-    return self._filename_without_ext
+  def stem(self):
+    """Get the liquid's natural name without an extension"""
+    return self._stem
 
   @property
-  def fileext(self):
-    return self._fileext
+  def suffix(self):
+    """Get the liquid's natural extension (without the dot)"""
+    return self._suffix
+
+  @property
+  def contents(self):
+    """Get the liquid's entire contents
+
+    This recklessly loads the liquid's entire contents to memory and
+    stores them. Use `open` for processing big files.
+    """
+
+    if not self._cached_contents is None:
+      return self._cached_contents
+
+    with self.open() as stream:
+      self._cached_contents = stream.read()
+
+    self._reader = lambda: io.BytesIO(self._cached_contents)
+
+    return self._cached_contents
+
+  def write_to(self, path):
+    """Write the liquid's contents to a file on disk"""
+    Path(path).write_bytes(self.contents)
 
   def open(self):
+    """Open the liquid's stream for reading"""
     return self._reader()
