6f4f2f113821f37196767bb648acaea88636d94e
sides <sides@sides.tv>
2018-11-29 00:12:35 +0000

    merge_jass: Cosmetics; better validations

--- war3archiver/mixer/merge_jass.py
+++ war3archiver/mixer/merge_jass.py
@@ -11,19 +11,15 @@ def merge_jass(ljass, rjass):
   hand's IDs unless specified otherwise via comment flags.
   """
 
-  l_ast = JassParser.parse(ljass)
-  r_ast = JassParser.parse(rjass)
+  destination = JassParser.parse(ljass)
+  incoming = JassParser.parse(rjass)
 
-  l_comments = JassParser.parse_comments(ljass)
-  r_comments = JassParser.parse_comments(rjass)
-
-  l_ids = set(l_ast)
-  r_ids = set(r_ast)
+  comments = JassParser.parse_comments(rjass)
 
   # Go through each node that needs merging
-  for id_ in l_ids.intersection(r_ids):
-    r_node = r_ast[id_]
-    lineno = r_node.id.line
+  for id_ in set(destination).intersection(set(incoming)):
+    incoming_node = incoming[id_]
+    lineno = incoming_node.id.line
 
     if id_ in ['main', 'config']:
       # Merge main and config by default
@@ -32,63 +28,62 @@ def merge_jass(ljass, rjass):
       strategy = 'rename'
 
     # Get a potential override from comments
-    if lineno in r_comments:
-      match = re.match(r'^/// merge:(.+)', r_comments[lineno])
+    if lineno in comments:
+      match = re.match(r'^/// merge:(.+)', comments[lineno])
 
       if not match is None:
-        flags = utils.parse_one_liner_flags(match[1])
+        strategy = match[1]
 
-        if 'rename' in flags:
-          strategy = 'rename'
-        elif 'lose' in flags:
-          strategy = 'lose'
-        elif 'win' in flags:
-          strategy = 'win'
-        elif 'merge' in flags:
-          if id_ not in ['main', 'config']:
-            raise Exception('Merge strategy "merge" may only be performed on the main or config functions, "%s" given (line %s)' % (
-              r_node.id.value, lineno))
-
-          strategy = 'merge'
+        if strategy == 'merge':
+          if id_ != 'main' and id_ != 'config':
+            raise Exception('Merge strategy "merge" may only be performed on the main or config functions: "%s" given (line %s)' % (
+              incoming_node.id.value, lineno))
+        elif strategy == 'win' or strategy == 'lose':
+          destination_node = destination[id_]
+          if not type(destination_node) is type(incoming_node):
+            raise Exception('Merge strategy "%s" may only be performed on the same type of node: "%s" and "%s" differ for "%s" (line %s)' % (
+              strategy, destination_node.data, incoming_node.data, incoming_node.id.value, lineno))
+        elif strategy != 'rename' and strategy != 'ignore':
+          raise Exception('Unknown merge strategy "%s" (line %s)' % (strategy, lineno))
 
     # Merge the nodes
     if strategy == 'rename':
-      l_ast.rename(id_, id_ + '__merged_1')
-      r_ast.rename(id_, id_ + '__merged_2')
+      destination.rename(id_, id_ + '__merged_1')
+      incoming.rename(id_, id_ + '__merged_2')
     elif strategy == 'win':
-      l_ast[id_] = r_node
-      del r_ast[id_]
+      destination[id_] = incoming_node
+      del incoming[id_]
     elif strategy == 'lose':
-      del r_ast[id_]
+      del incoming[id_]
     elif strategy == 'merge':
-      l_node = l_ast[id_]
+      destination_node = destination[id_]
 
       suffix = 1
-      for func in [l_node, r_node]:
+      for func in [destination_node, incoming_node]:
         for local in func.locals:
           func.rename_local(local.id.value, local.id.value + '__merged_%s' % suffix)
 
         suffix += 1
 
-      l_node.locals += r_node.locals
-      l_node.statements += r_node.statements
+      destination_node.locals += incoming_node.locals
+      destination_node.statements += incoming_node.statements
 
-      del r_ast[id_]
+      del incoming[id_]
 
   # Concat the rest
-  l_ast.types += r_ast.types
-  l_ast.natives += r_ast.natives
-  l_ast.globals += r_ast.globals
-  l_ast.functions += r_ast.functions
+  destination.types += incoming.types
+  destination.natives += incoming.natives
+  destination.globals += incoming.globals
+  destination.functions += incoming.functions
 
   # Move main and config to bottom
-  if 'main' in l_ast:
-    main_ = l_ast['main']
-    l_ast.replace(main_, None)
-    l_ast.functions += [main_]
-  if 'config' in l_ast:
-    config = l_ast['config']
-    l_ast.replace(config, None)
-    l_ast.functions += [config]
+  if 'main' in destination:
+    main = destination['main']
+    destination.replace(main, None)
+    destination.functions += [main]
+  if 'config' in destination:
+    config = destination['config']
+    destination.replace(config, None)
+    destination.functions += [config]
 
-  return JassParser.build(l_ast)
+  return JassParser.build(destination)
